e987b486fa5a0d6d763c532b5f2351030a4c3b7c
Gary Menezes <gary@brkt.com>
2014-07-28 00:05:51 -0700

    Mended fixtures.

--- app/api/fixtures/fix.py
+++ app/api/fixtures/fix.py
@@ -1,12 +1,34 @@
 import json
-out = ""
-with open("initial_data.json", "r") as f:
-  counter = 0
-  for l in f:
-    if '"pk": "null"' in l:
-      out += '"pk": %s\n' % counter
-      counter += 1
-    else:
-      out += l
+import requests
+from xml.dom import minidom
+
+jsonin = None
+jsonout = []
+with open("institutions", "r") as f:
+  jsonin = json.loads(f.read())
+
+counter = 0
+seen_fids = set()
+for i in jsonin:
+  data = requests.get('http://www.ofxhome.com/api.php?lookup=%s' % i['fields']['fid'])
+  try:
+    xml = minidom.parseString(data.text)
+    name = xml.getElementsByTagName('name')[0].firstChild.nodeValue
+    fid = xml.getElementsByTagName('fid')[0].firstChild.nodeValue
+    org = xml.getElementsByTagName('org')[0].firstChild.nodeValue
+    if fid in seen_fids:
+      print 'repeat fid: %s' % name
+      continue
+    seen_fids.add(fid)
+    i['fields']['name'] = name
+    i['fields']['fid'] = fid
+    i['fields']['organization'] = org
+    i['pk'] = counter
+    counter += 1
+    jsonout.append(i)
+    print name
+  except:
+    pass
+
 with open("out.json", "w") as o:
-  o.write(json.dumps(json.loads(out), indent=2, separators=(',', ': ')))
+  o.write(json.dumps(jsonout, indent=2, separators=(',', ': ')))
